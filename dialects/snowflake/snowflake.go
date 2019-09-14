9c77e5cac46d0eb3e393a67161536a7d72a1ba0c
dc-dos <dconnell@dos.sh>
2019-09-14 12:36:51 -0400

    documentation

--- dialects/snowflake/snowflake.go
+++ dialects/snowflake/snowflake.go
@@ -1,5 +1,59 @@
 package snowflake
 
+/*/
+	Snowflake dialect provides the VARIANT data type. This is a snowflake column type that can
+	resolve structs, maps and arrays. The dialect also supports defining columns of type 
+	snowflake.Object or snowflake.Array specifically (but under the hood, it's all handled as
+	VARIANTs and we let snowflake manage the rest). 
+	
+	In order to get things to work with arbitrary semi-structured data, all values are basically
+	reduced to JSON values in a []byte. A set of helper functions allow the mashalling of the data
+	in and out of the VARIANT type into specific data stuctures.
+
+	Example: 
+		// create model with VARIANT column
+		type Widget struct {
+			ID  string            	`gorm:"column:ID;size:36"`
+			Item snowflake.Variant 	`gorm:"column:ITEM;type:VARIANT"`
+			CreatedAt time.Time 	`gorrm:'column:CREATED_AT:TIMESTAMP_TZ`
+			UpdatedAt time.Time 	`gorrm:'column:CREATED_AT:TIMESTAMP_TZ`
+		}
+
+		// create data map for Item 
+		// (structs/arrays work too)
+		itm := map[string]string{
+			"Name":  "Bob A. Fett",
+			"Address": "123 Somewhere",
+			"Email": "Nah@notthere.net",
+			"Company": "Empire Bounty, Inc",
+		}
+
+		// wrap in Variant
+		var vitm snowlake.Variant
+		if err := snowflake.NewVariant(&vitm); err != nil {
+			// json error 
+			panic(err)
+		}
+		
+		// Insert new widget for that uses 
+		// the variant column value
+		db.Create(&Widget{
+			ID: iid,
+			Item: vitm
+		})
+
+		// Read it back
+		var widget Widget
+		db.First(&widget, "\"ID\" = ?", iid) // find new widget
+
+		// extract item from widget
+		if err = widget.Item.Get(&itm); err == nil {
+			// and done
+			fmt.Println(itm)
+			}
+		}
+/*/
+
 import (
 	"database/sql/driver"
 	"encoding/json"
@@ -33,8 +87,8 @@ func (o *Variant) Scan(value interface{}) error {
 // NewVariant create a new Variant type
 func NewVariant(v interface{}) (Variant, error) {
 	var o Variant
-	err := o.Set(v)
- 	return o, err
+	err := o.Set(v) 
+	return o, err
 }
 
 // Set loads a new type into the Variant wrapper
@@ -44,12 +98,32 @@ func (o *Variant) Set(value interface{}) (err error) {
 }
 
 // Get extracts the innner struct as an interface
-func (o Variant) Get(value interface{}) (interface{}, error) {
-	err := json.Unmarshal(o.value, value)
-	return value, err
+func (o Variant) Get(value interface{}) (err error) {
+	err = json.Unmarshal(o.value, value)
+	return 
 }
 
 // String returns string representation of value
 func (o Variant) String() string {
 	return fmt.Sprintf("%s", o.value)
+}
+
+// Object is an alias for 
+// the snowflake Variant type.
+type Object Variant
+
+// NewObject returns a new Variant that
+// can manage structs, maps and arrays
+func NewObject(v interface{}) (Variant, error) {
+	return NewVariant(v)
+}
+
+// Array is an alias for the 
+// snowflake Variant type  
+type Array Variant
+
+// NewArray returns a new Variant that
+// can manage structs, maps and arrays
+func NewArray(v interface{}) (Variant, error) {
+	return NewVariant(v)
 }
